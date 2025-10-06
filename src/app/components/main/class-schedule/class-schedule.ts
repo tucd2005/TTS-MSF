4047d178f10a37e4a66371e2a786b941cb620add
tucd2005 <tucdph51409@gmail.com>
2025-10-06 11:20:08 +0700

    done--lịch học

--- src/app/components/main/class-schedule/class-schedule.ts
+++ src/app/components/main/class-schedule/class-schedule.ts
@@ -8,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrl: './class-schedule.scss'
 })
 export class ClassSchedule {
+  days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 
+  timeSlots = [
+    {
+      time: '15:20',
+      date: '30/08/2025',
+      missions: [
+        { text: '', dot: '#aaa' },
+        { text: '', dot: '#aaa' },
+        { text: '', dot: '#aaa' },
+        { text: '', dot: '#aaa' },
+        { text: '', dot: '#aaa' },
+      ]
+    },
+    {
+      time: '15:20',
+      date: '30/08/2025',
+      missions: [
+        { text: 'Tiết toán - Clan', dot: '#0070f3' },
+        { text: '', dot: '#aaa' },
+        { text: 'Nộp bài kiểm tra', dot: '#ff0000' },
+        { text: '', dot: '#aaa' },
+        { text: '', dot: '#aaa' },
+      ]
+    },
+    {
+      time: '15:20',
+      date: '30/08/2025',
+      missions: [
+        { text: '', dot: '#aaa' },
+        { text: '', dot: '#aaa' },
+        { text: 'Nộp bài kiểm tra', dot: '#ff0000' },
+        { text: '', dot: '#aaa' },
+        { text: '', dot: '#aaa' },
+      ]
+    },
+  ];
 }
