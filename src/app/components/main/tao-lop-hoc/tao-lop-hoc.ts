2f3ec407416bd392ce0f31457d954a1f20184270
tucd2005 <tucdph51409@gmail.com>
2025-10-15 09:26:50 +0700

    ok ổn rồi

---
+++ src/app/components/main/tao-lop-hoc/tao-lop-hoc.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tao-lop-hoc',
+  imports: [],
+  templateUrl: './tao-lop-hoc.html',
+  styleUrl: './tao-lop-hoc.scss'
+})
+export class TaoLopHoc {
+
+}
